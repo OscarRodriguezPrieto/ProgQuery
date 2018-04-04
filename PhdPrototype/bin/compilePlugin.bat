1377dbd90244e07422420858b8db92b02ba79acc
Oscar <oscar.rodriguez.prieto@gmail.com>
2018-04-04 17:22:30 +0200

    Recovering deleted files.

---
+++ PhdPrototype/bin/compilePlugin.bat
@@ -0,0 +1,5 @@
+rmdir /Q /S bin\examples
+jar cfe Wiggle.jar WiggleIndexerPlugin -C bin/ . build\libs\*
+del build\libs\Wiggle.jar
+xcopy Wiggle.jar build\libs\Wiggle.jar  < file.txt
+del Wiggle.jar
