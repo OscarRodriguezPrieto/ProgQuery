2b220dfb539de96f314123060714ca49e318a5c1
Oscar <oscar.rodriguez.prieto@gmail.com>
2018-06-21 15:31:16 +0200

    Added README file.

--- PhdPrototype/classZ.bat
+++ PhdPrototype/classZ.bat
@@ -1,2 +1,2 @@
-javac -cp src;build/libs/neo4j/*; -Xplugin:WiggleIndexerPlugin -d bon src\examples\pdg\A.java
+javac -cp src;build/libs/neo4j/*; -Xplugin:ProQueryPlugin -d bon src\examples\pdg\A.java
 pause
