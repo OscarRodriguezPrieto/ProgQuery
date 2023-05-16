e8d0e1da0055ed0ca008a93e178a31c3a66d0ecf
Oscar Rodriguez Prieto <oscar.rodriguez.prieto@gmail.com>
2023-05-16 15:04:49 +0200

    Substituing VarSymbol.isLocal invocation, which is not available in Java 17, for a private method to calculate the same.

--- src/main/java/es/uniovi/reflection/progquery/visitors/IsInstanceFieldExpression.java
+++ src/main/java/es/uniovi/reflection/progquery/visitors/IsInstanceFieldExpression.java
@@ -11,33 +11,42 @@ import com.sun.tools.javac.code.Symbol.VarSymbol;
 import com.sun.tools.javac.tree.JCTree.JCFieldAccess;
 import com.sun.tools.javac.tree.JCTree.JCIdent;
 
+import javax.lang.model.element.ElementKind;
+
 //RETURNS THE CLASS SYMBOL OF THE LEFT MOST (VALID- NON CLASS SYMBOL) ID IN THE LEFT PAQRT OF ASSIGNMENT
 public class IsInstanceFieldExpression extends TreeScanner<Boolean, Void> {
-	public static final IsInstanceFieldExpression GET_ID_VISITOR = new IsInstanceFieldExpression();
+    public static final IsInstanceFieldExpression GET_ID_VISITOR = new IsInstanceFieldExpression();
 
-	@Override
-	public Boolean scan(Tree t, Void v) {
-		Boolean res = super.scan(t, v);
-		return res == null ? false : res;
-	}
+    @Override
+    public Boolean scan(Tree t, Void v) {
+        Boolean res = super.scan(t, v);
+        return res == null ? false : res;
+    }
 
-	@Override
-	public Boolean visitIdentifier(IdentifierTree identTree, Void v) {
-		Symbol s = ((JCIdent) identTree).sym;
-		return !(s.kind == Kinds.Kind.VAR && ((VarSymbol) s).isLocal());
-	}
+    private boolean isLocal(VarSymbol symbol) {
+        return symbol.getKind() == ElementKind.EXCEPTION_PARAMETER || symbol.getKind() == ElementKind.LOCAL_VARIABLE ||
+                symbol.getKind() == ElementKind.PACKAGE || symbol.getKind() == ElementKind.PARAMETER ||
+                symbol.getKind() == ElementKind.RESOURCE_VARIABLE;
+    }
 
-	@Override
-	public Boolean visitMemberSelect(MemberSelectTree memberSel, Void v) {
-		Symbol s = ((JCFieldAccess) memberSel).sym;
-		return s.kind == Kinds.Kind.VAR && s.isStatic() ? false : scan(memberSel.getExpression(), v);
+    @Override
+    public Boolean visitIdentifier(IdentifierTree identTree, Void v) {
+        Symbol s = ((JCIdent) identTree).sym;
+        //Before we used VarSymbol.isLocal, but in java 17 is not available any more
+        return !(s.kind == Kinds.Kind.VAR && isLocal((VarSymbol) s));
+    }
 
-	}
+    @Override
+    public Boolean visitMemberSelect(MemberSelectTree memberSel, Void v) {
+        Symbol s = ((JCFieldAccess) memberSel).sym;
+        return s.kind == Kinds.Kind.VAR && s.isStatic() ? false : scan(memberSel.getExpression(), v);
 
-	@Override
-	public Boolean visitArrayAccess(ArrayAccessTree arrayAccess, Void v) {
-		return scan(arrayAccess.getExpression(), v);
+    }
 
-	}
+    @Override
+    public Boolean visitArrayAccess(ArrayAccessTree arrayAccess, Void v) {
+        return scan(arrayAccess.getExpression(), v);
+
+    }
 
 }
